bed7b540729b98c09d8b1cc1122ab18a64ec1b88
HarishPJ21 <38295998+HarishPJ21@users.noreply.github.com>
2023-08-11 22:35:42 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import {  EditTodo,TodoListItem,CreateTodo,PostDetail } from './'; 
+import { addTodo, deleteTodo, fetchTodo, updateTodo } from '../ApiCalls';
+
+
+function App() {
+  let new_id=201;
+  const [todoItems, setTodoItems] = useState([]);
+  const [editValue,setEditValue]=useState();
+  const [edit,setEdit]=useState(false);
+  console.log("edit:",edit);
+
+  function addItem(title){
+    const newTodoItem = {
+      "userId": 1,
+      "id": new_id++,
+      "title": title,
+      "completed": false
+    }
+    console.log("newitem:",newTodoItem);  
+    setTodoItems([newTodoItem,...todoItems]);
+    console.log("additem:",todoItems);  
+    addTodo(newTodoItem);
+  }
+  function deleteItem(todoID){
+    setTodoItems(todoItems.filter((todoItem)=>todoItem.id!==todoID));
+    deleteTodo(todoID);
+  }
+  function editItem(todoID){
+    const ev=todoItems.find((todoItem)=>todoItem.id===todoID);
+    console.log("ev:",ev)
+    setEditValue(ev);
+    setEdit(true);
+
+    // setTodoItems()
+  }
+  function save(todoID, updatedTitle){
+    console.log("save");
+    setTodoItems(todoItems.map((todoItem)=>{
+      if(todoItem.id===todoID)
+       todoItem.title = updatedTitle 
+      return todoItem;
+    }));
+    setEdit(false);
+    updateTodo(todoID);
+  }
+  function cancel(){
+    console.log("cancel");
+    setEdit(false);
+  }  
+
+
+  function completeItem(todoID){
+    // console.log("completeItem:", todoID);
+    setTodoItems(todoItems.map((todoItem)=>{
+      if(todoItem.id===todoID)
+       todoItem.completed = !todoItem.completed 
+      return todoItem;
+    }));
+  }
+
+  useEffect(() => {
+    const url="https://jsonplaceholder.typicode.com/todos/?userId=1";
+    fetch(url).then((Response)=>Response.json()).then((data)=>{
+      data;
+      setTodoItems(data);
+    })
+    // setTodoItems(fetchTodo())
+    console.log("useEffect:",todoItems);
+  }, []);
+      // editTodoValue=todoItems.filter((todoItem)=>todoItem.id===todoID);
+    // console.log("editTodoValue:");  
+  return (
+    <div className="container">
+      {edit && <EditTodo editValue={editValue} save={save} cancel={cancel}/>}
+      {!edit && <CreateTodo addItem={addItem} />}
+      <TodoListItem todoItems={todoItems} completeItem={completeItem} deleteItem={deleteItem} editItem={editItem}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TodoListItem from "./TodoListItem";
+import CreateTodo from "./CreateTodo";
+import EditTodo from "./EditTodo";
+
+export{ TodoListItem,CreateTodo,EditTodo }; 
